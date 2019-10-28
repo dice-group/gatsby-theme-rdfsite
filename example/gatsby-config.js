f355e02b3becda1c322a5cc29f8f1eb47178cb65
Tim Ermilov <yamalight@gmail.com>
2019-10-28 12:22:16 +0000

    Allow defining social links from config

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     description: `Example RDF website generated with Gatsby`,
     author: `@yamalight`,
     footer: `Example footer text`,
+    social: {
+      twitter: 'https://twitter.com/test',
+      github: 'https://github.com/test',
+      facebook: '',
+      youtube: '',
+      instagram: '',
+    },
   },
   plugins: [{ resolve: `gatsby-theme-rdfsite`, options: {} }],
 };
--- gatsby-theme-rdfsite/src/components/social/index.js
+++ gatsby-theme-rdfsite/src/components/social/index.js
@@ -1,41 +1,71 @@
-import React from 'react';
-import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { useStaticQuery } from 'gatsby';
+import _ from 'lodash';
+import React, { useMemo } from 'react';
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
-const links = [
-  { url: 'https://twitter.com/knowgraphs', text: 'Twitter', icon: FaTwitter },
-  {
-    url: 'https://github.com/KnowGraphs',
-    text: 'Github',
-    icon: FaGithub,
-  },
-];
+const icons = {
+  twitter: FaTwitter,
+  github: FaGithub,
+  facebook: FaFacebook,
+  youtube: FaYoutube,
+  instagram: FaInstagram,
+};
 
-const Social = ({ style, className, hiddenMobile = true }) => (
-  <div
-    className={`column is-flex social ${
-      hiddenMobile ? 'is-hidden-mobile' : ''
-    }`}
-    style={{
-      position: 'fixed',
-      left: 0,
-      top: 96,
-      height: 'calc(60% - 96px)',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column',
-      maxWidth: 50,
-      margin: '0.5em',
-      ...style,
-    }}
-  >
-    {links.map(l => (
-      <a key={l.url} href={l.url} title={l.text}>
-        <span className="icon is-large">
-          <l.icon size={25} />
-        </span>
-      </a>
-    ))}
-  </div>
-);
+const Social = ({ style, className, hiddenMobile = true }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            social {
+              twitter
+              github
+              facebook
+              youtube
+              instagram
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const links = useMemo(
+    () =>
+      Object.keys(site.siteMetadata.social)
+        .filter(key => site.siteMetadata.social[key] && site.siteMetadata.social[key].length > 0)
+        .map(key => ({
+          url: site.siteMetadata.social[key],
+          text: _.capitalize(key),
+          icon: icons[key],
+        })),
+    []
+  );
+
+  return (
+    <div
+      className={`column is-flex social ${hiddenMobile ? 'is-hidden-mobile' : ''}`}
+      style={{
+        position: 'fixed',
+        left: 0,
+        top: 96,
+        height: 'calc(60% - 96px)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        maxWidth: 50,
+        margin: '0.5em',
+        ...style,
+      }}>
+      {links.map(l => (
+        <a key={l.url} href={l.url} title={l.text}>
+          <span className="icon is-large">
+            <l.icon size={25} />
+          </span>
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default Social;
