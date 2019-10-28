6c94f0126b79a0ce91a1d52de58edac52ac01e56
Tim Ermilov <yamalight@gmail.com>
2019-10-28 12:13:19 +0000

    Use project logo as image placeholder by default

--- gatsby-theme-rdfsite/src/components/image.js
+++ gatsby-theme-rdfsite/src/components/image.js
@@ -1,7 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import React from 'react';
-import KG from './svgs/knowgraphs-logo.inline.svg';
 
 const Image = ({ filename, alt, style, className = 'image' }) => {
   // pre-calculate all images data
@@ -24,27 +23,25 @@ const Image = ({ filename, alt, style, className = 'image' }) => {
   `);
 
   // find image that user wanted by matching path end
-  const imageEl =
+  let imageEl =
     filename && filename.length > 0
       ? images.find(element => element.node.fluid.src.endsWith(`/${filename}`))
       : undefined;
 
+  // if image is not found - default to project logo
   if (!imageEl) {
-    return (
-      <KG height="100%" width="100%" preserveAspectRatio="xMidYMid meet" />
-    );
+    const projectLogo = images.find(element => element.node.fluid.src.endsWith(`/site-icon.png`));
+
+    // if project logo not found - default to empty span
+    if (!projectLogo) {
+      return <span />;
+    }
+
+    imageEl = projectLogo;
   }
 
   const image = imageEl.node.fluid;
-  return (
-    <Img
-      className={className}
-      fluid={image}
-      objectFit="cover"
-      alt={alt}
-      style={style}
-    />
-  );
+  return <Img className={className} fluid={image} objectFit="cover" alt={alt} style={style} />;
 };
 
 export default React.memo(Image);
