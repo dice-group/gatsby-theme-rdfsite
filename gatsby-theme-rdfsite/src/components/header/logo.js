5bb0edc3ecfefda233eb251390dea9b56c5e5ef3
Tim Ermilov <yamalight@gmail.com>
2019-11-01 12:04:58 +0000

    Use fixed width for default logo image

--- gatsby-theme-rdfsite/src/components/header/logo.js
+++ gatsby-theme-rdfsite/src/components/header/logo.js
@@ -3,7 +3,12 @@ import Image from '../image';
 
 export default ({ site }) => (
   <>
-    <Image filename="site-icon.png" alt="Site logo" className="dice-nav-logo" />
+    <Image
+      filename="site-icon.png"
+      alt="Site logo"
+      className="dice-nav-logo"
+      style={{ width: 50 }}
+    />
     <span className="ml-3">{site.siteName}</span>
   </>
 );
