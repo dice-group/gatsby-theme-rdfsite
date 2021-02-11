5b82850d3dcd9bead9a37a0e55202d4a0bb114b1
Tim Ermilov <yamalight@gmail.com>
2021-02-11 11:50:24 +0100

    Allow using SVGs as images

--- gatsby-theme-rdfsite/src/components/image.js
+++ gatsby-theme-rdfsite/src/components/image.js
@@ -8,8 +8,18 @@ const Image = ({ filename, alt, style, className = 'image' }) => {
   // dynamic queries on the build time
   const {
     allImageSharp: { edges: images },
+    svgs: { edges: svgs },
   } = useStaticQuery(graphql`
     query {
+      svgs: allFile(filter: { extension: { eq: "svg" } }) {
+        edges {
+          node {
+            extension
+            publicURL
+          }
+        }
+      }
+
       allImageSharp {
         edges {
           node {
@@ -25,12 +35,24 @@ const Image = ({ filename, alt, style, className = 'image' }) => {
   // find image that user wanted by matching path end
   let imageEl =
     filename && filename.length > 0
-      ? images.find(element => element.node.fluid.src.endsWith(`/${filename}`))
+      ? images.find((element) =>
+          element.node.fluid.src.endsWith(`/${filename}`)
+        )
+      : undefined;
+
+  // find svg that user wanted by matching public URL end
+  const svgEl =
+    filename && filename.length > 0
+      ? svgs.find(
+          (element) =>
+            element.node.publicURL.includes(filename.replace('.svg', '')) &&
+            element.node.publicURL.endsWith('.svg')
+        )
       : undefined;
 
   // if image is not found - default to project logo
-  if (!imageEl) {
-    const projectLogo = images.find(element =>
+  if (!imageEl && !svgEl) {
+    const projectLogo = images.find((element) =>
       element.node.fluid.src.endsWith(`/site-icon.png`)
     );
 
@@ -42,6 +64,13 @@ const Image = ({ filename, alt, style, className = 'image' }) => {
     imageEl = projectLogo;
   }
 
+  // render svg if present
+  if (svgEl) {
+    const svgPath = svgEl.node.publicURL;
+    return <img src={svgPath} alt={alt} style={style} />;
+  }
+
+  // render image otherwise
   const image = imageEl.node.fluid;
   return (
     <Img
