2b960206536a0345efe86394300c7a06733d17b1
Micha <michael.roeder@uni-paderborn.de>
2021-05-04 15:08:04 +0200

    Added the functionality to make use of a schema:url to link the title of papers in case schema:pdfUrl is not given.

--- gatsby-theme-rdfsite/src/components/papers/paper.js
+++ gatsby-theme-rdfsite/src/components/papers/paper.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ExternalLink from '../externalLink';
 
+const renderTitle = ({ title, url, pdfUrl }) => {
+  if (pdfUrl) {
+    return <ExternalLink to={pdfUrl}>{title}</ExternalLink>;
+  }
+  if (url) {
+    return <ExternalLink to={url}>{title}</ExternalLink>;
+  }
+
+  return title;
+};
+
 const Paper = ({
   data: {
     source,
@@ -15,7 +26,7 @@ const Paper = ({
   <div className="paper">
     <p className="paper-text">{source}</p>
     <h3 className="paper-name">
-      {pdfUrl ? <ExternalLink to={pdfUrl}>{title}</ExternalLink> : title}
+      {renderTitle({ title, url, pdfUrl })}{' '}
     </h3>
     <p className="paper-meta">By {(authorName || []).join(', ')}</p>
     <p className="paper-meta">
